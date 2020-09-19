9084e4c0945a6d8af345a536230ed87279115b17
Amanda Rolon <abrolon87@gmail.com>
2020-09-18 20:13:50 -0400

    deletes state; brings in props

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 
 export class UserItem extends Component {
-  state = {
-    id: "id",
-    login: "github user",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/abrolon87",
-  };
-
   render() {
-    const { login, avatar_url, html_url } = this.state;
+    const { login, avatar_url, html_url } = this.props.user;
     return (
       <div className="card text-center">
         <img
