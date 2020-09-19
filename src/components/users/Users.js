9069c068ef96b63882216850127a4d6e344c6fa5
Amanda Rolon <abrolon87@gmail.com>
2020-09-19 00:38:44 -0400

    deletes hard coded data and state; brings in props with real data from api

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,33 +2,10 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "github user",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/abrolon87",
-      },
-      {
-        id: "2",
-        login: "github user",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/abrolon87",
-      },
-      {
-        id: "3",
-        login: "github user",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/abrolon87",
-      },
-    ],
-  };
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
