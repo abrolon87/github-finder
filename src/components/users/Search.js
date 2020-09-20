9b93adfa962938d23b1b4d8d4eaf287cc61f4104
Amanda Rolon <abrolon87@gmail.com>
2020-09-20 12:56:18 -0400

    refactoring

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -23,6 +23,7 @@ export class Search extends Component {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -46,11 +47,8 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
-        {this.props.showClear && (
-          <button
-            className="btn btn-light btn-block"
-            onClick={this.props.clearUsers}
-          >
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
             Clear Results
           </button>
         )}
