ee9a5b76bac5a7f13b75711db98a90e81797b685
Amanda Rolon <abrolon87@gmail.com>
2020-09-20 22:34:02 -0400

    adds Alert functionality for when there is no input in search field

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -11,6 +11,7 @@ export class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value }); //this works with multiple fields
@@ -18,8 +19,12 @@ export class Search extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: "" });
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
   };
 
   render() {
