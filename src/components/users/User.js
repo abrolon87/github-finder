8f51c236c2a6c907e5e2f03a408ca417caeeaea1
Amanda Rolon <abrolon87@gmail.com>
2020-09-22 18:16:58 -0400

    adds noopener and no referrer

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -64,7 +64,12 @@ export class User extends Component {
                 <p>{bio}</p>
               </Fragment>
             )}
-            <a href={html_url} className="btn btn-dark my-1" target="_blank">
+            <a
+              href={html_url}
+              className="btn btn-dark my-1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Visit Github Profile
             </a>
             <ul>
