2a657e347b9b2921c65c225ceacc78bdb2c7045d
Amanda Rolon <abrolon87@gmail.com>
2020-09-22 15:51:09 -0400

    imports Spinner, PropTypes, and Link; adds propTypes, sets Spinner to display while loading; adds back button

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,18 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
 
   render() {
     const {
@@ -22,10 +31,14 @@ export class User extends Component {
     } = this.props.user;
 
     const { loading } = this.props;
+
+    if (loading) return <Spinner />;
     return (
-      <div>
-        <p>{name}</p>
-      </div>
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back to Search
+        </Link>
+      </Fragment>
     );
   }
 }
