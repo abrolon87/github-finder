19f0bd07aae48e43b8db6ed50acc5a168cef935a
Amanda Rolon <abrolon87@gmail.com>
2020-09-22 22:24:29 -0400

    converts to functional component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,65 +1,61 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+
 import PropTypes from "prop-types";
 
-export class Search extends Component {
-  state = {
-    text: "",
-    // email: "",
-  };
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+  const [text, setText] = useState("");
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  };
-
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value }); //this works with multiple fields
+  const onChange = (e) => setText(e.target.value); //this works with multiple fields
   // this.setState({ text: e.target.value }); this works with a single field
 
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert("Please enter something", "light");
+    if (text === "") {
+      setAlert("Please enter something", "light");
     } else {
-      this.props.searchUsers(this.state.text);
+      searchUsers(text);
       this.setState({ text: "" });
+      setText("");
     }
   };
 
-  render() {
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input
-            type="text"
-            name="text" //e.target.name refers to this name
-            placeholder="Search Users..."
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          {/* <input
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text" //e.target.name refers to this name
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        {/* <input
             type="email"
             name="email" //e.target.name refers to this name
             placeholder="Search email..."
             value={this.state.email}
             onChange={this.onChange}
           /> */}
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {showClear && (
-          <button className="btn btn-light btn-block" onClick={clearUsers}>
-            Clear Results
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear Results
+        </button>
+      )}
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
 
 export default Search;
